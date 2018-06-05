a28ab926bc95cf5ef5f7d8a6f94fe270d435d1e6
Natasha K <kapr.natasha@gmail.com>
2018-06-05 23:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   item = { itemName: `${item}`, itemPrice: Math.floor(Math.random()*100) }
-  return '${itemName} has been added to your cart.'
+  return `${itemName} has been added to your cart.`
 }
 
 
