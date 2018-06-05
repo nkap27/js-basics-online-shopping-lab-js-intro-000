37d8da52b004057b4444bd923ad91f38e33b1307
Natasha K <kapr.natasha@gmail.com>
2018-06-05 23:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,9 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
   }
-  
 }
 
 function total() {
