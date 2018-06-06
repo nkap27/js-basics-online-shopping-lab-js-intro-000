9847c7714d68b314f8ff0395f8ff9160a1b10fe9
Natasha K <kapr.natasha@gmail.com>
2018-06-06 00:03:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
     for(var i = 0; i < cart.length - 1; i++) {
       myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
-    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length].itemPrice}.`
+    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
   }
 }
 
