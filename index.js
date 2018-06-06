117a0c7f003b647aeceacb59a1dc545e2caff1d3
Natasha K <kapr.natasha@gmail.com>
2018-06-06 00:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,6 +62,9 @@ function placeOrder(cardNumber) {
 
   if (cardNumber = "") {
     return "Sorry, we don't have a credit card on file for you."
+  } else {
+    cart = [];
+    return `Your total cost is ${total()}, which will be charged to the ${cardNumber}.`
   }
   // write your code here
 }
